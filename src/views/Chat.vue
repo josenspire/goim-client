b16ff6c468922bb80507f455e9d501c4709e48c0
josenspire <josenspire@gmail.com>
2020-09-07 21:53:57 +0800

    initial project

---
+++ src/views/Chat.vue
@@ -0,0 +1,191 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <div class="left-panel">
+        <div class="title">
+          <input type="text" />
+        </div>
+        <div class="list-panel">
+          <div class="user-item">User1</div>
+          <div class="user-item">User2</div>
+          <div class="user-item">User3</div>
+          <div class="user-item">User4</div>
+          <div class="user-item">User5</div>
+          <div class="user-item">User4</div>
+          <div class="user-item">User5</div>
+          <div class="user-item">User4</div>
+          <div class="user-item">User5</div>
+        </div>
+      </div>
+      <div class="right-panel">
+        <div class="title">
+          <span>James</span>
+        </div>
+        <div class="view-panel">
+          <div class="msg-item left">
+            <a-icon type="user" /> <span>How are your ?</span>
+          </div>
+          <div class="msg-item right">
+            <span>I'm fine, thank you! And you?</span> <a-icon type="user" />
+          </div>
+          <div class="msg-item left">
+            <a-icon type="user" /> <span>Fine.</span>
+          </div>
+        </div>
+        <div class="ops-panel">
+          <div class="input-panel">
+            <a-textarea placeholder="Basic usage" :rows="1" />
+          </div>
+          <div class="footer">
+            <a-button type="primary">Send</a-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  .content {
+    border: 1px solid #f0f2f5;
+    color: #ffffff;
+    min-height: 120px;
+    display: flex;
+    min-width: 1024px;
+    height: 720px;
+    background-color: #f0f2f5;
+    .left-panel {
+      background-color: #fff;
+      height: 100%;
+      width: 35%;
+      min-width: 120px;
+      .title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #000;
+        opacity: 0.8;
+        height: 12%;
+        input {
+          width: 70%;
+          height: 40px;
+        }
+      }
+      .list-panel {
+        width: 100%;
+        height: 88%;
+        overflow-x: hidden;
+        overflow-y: auto;
+        .user-item {
+          background-color: #000;
+          color: #fff;
+          height: 80px;
+          line-height: 80px;
+          &:not(:first-child) {
+            margin: 5px 0 0 0;
+          }
+        }
+      }
+    }
+    .right-panel {
+      background-color: #f0f2f5;
+      width: 75%;
+      height: 100%;
+      .title {
+        font-size: 20px;
+        font-weight: 600;
+        text-align: left;
+        color: #000;
+        background-color: #fcfcfc;
+        height: 40px;
+        display: flex;
+        align-items: center;
+        padding: 10px 20px;
+      }
+      .view-panel {
+        height: 65%;
+        background-color: #fcfcfc;
+        overflow-x: hidden;
+        overflow-y: auto;
+        padding: 5px 10px;
+        border-top: 1px solid #f1f1f1;
+        border-bottom: 1px solid #f1f1f1;
+        .msg-item {
+          margin: 10px 0;
+        }
+        .left,
+        .right {
+          display: flex;
+          align-items: center;
+          width: 100%;
+          height: 40px;
+          i {
+            border: 1px solid #9c9c9c;
+            padding: 5px;
+            font-size: 24px;
+            color: #000;
+          }
+          span {
+            display: flex;
+            align-items: center;
+            height: 100%;
+            min-width: 70px;
+            padding: 0 10px;
+            border-radius: 0.4em;
+            background: green;
+            opacity: 0.8;
+          }
+        }
+        .left {
+          justify-content: flex-start;
+          span {
+            margin-left: 10px;
+          }
+        }
+        .right {
+          justify-content: flex-end;
+          span {
+            margin-right: 10px;
+          }
+        }
+      }
+      .ops-panel {
+        background-color: #ffffff;
+        height: calc(35% - 40px);
+        .input-panel {
+          width: 100%;
+          .ant-input {
+            width: 100%;
+            height: 160px;
+            border: none;
+            &:focus {
+              box-shadow: none;
+              border: none;
+            }
+          }
+        }
+        .footer {
+          width: 100%;
+          height: auto;
+          padding: 10px 10px;
+          text-align: right;
+        }
+      }
+    }
+  }
+}
+</style>
